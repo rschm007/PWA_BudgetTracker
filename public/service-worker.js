5ae8ba70451c9dad7f2fc485a59506372f852218
Robert Schmahl <rschm007@gmail.com>
2021-01-29 17:57:02 -0800

    expanded serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,8 @@ const FILES_TO_CACHE = [
   "styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
 // install
