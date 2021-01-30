c27c025d1fddf455f857a6f83bfff251284232e6
Robert Schmahl <rschm007@gmail.com>
2021-01-29 18:17:47 -0800

    expanded manifest to include model

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "index.js",
   "styles.css",
+  "./models/transaction.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
