8fce7ccfe586dc6f26dbdfc8594006f536ab000d
Robert Schmahl <rschm007@gmail.com>
2021-01-31 13:38:53 -0800

    updated service worker routes & added db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,79 +5,71 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/styles.css",
-  "./models/transaction.js",
+  "/db.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
-const STATIC_CACHE = `static-cache-v1`;
-const RUNTIME_CACHE = `runtime-cache`;
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener(`install`, (event) => {
+// install
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches
-      .open(STATIC_CACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener(`activate`, (event) => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+// activate
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) =>
-        // return array of cache names that are old to delete
-        cacheNames.filter((cacheName) => !currentCaches.includes(cacheName))
-      )
-      .then((cachesToDelete) =>
-        Promise.all(
-          cachesToDelete.map((cacheToDelete) => caches.delete(cacheToDelete))
-        )
-      )
-      .then(() => self.clients.claim())
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
-self.addEventListener(`fetch`, (event) => {
-  if (
-    event.request.method !== `GET` ||
-    !event.request.url.startsWith(self.location.origin)
-  ) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
+// fetch
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    console.log("[Service Worker] Fetch(data)", event.request.url);
 
-  if (event.request.url.includes(`/api/transaction`)) {
     event.respondWith(
-      caches.open(RUNTIME_CACHE).then((cache) =>
-        fetch(event.request)
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(event.request)
           .then((response) => {
-            cache.put(event.request, response.clone());
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
             return response;
           })
-          .catch(() => caches.match(event.request))
-      )
+          .catch((err) => {
+            return cache.match(event.request);
+          });
+      })
     );
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return caches
-        .open(RUNTIME_CACHE)
-        .then((cache) =>
-          fetch(event.request).then((response) =>
-            cache.put(event.request, response.clone()).then(() => response)
-          )
-        );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
     })
   );
 });
